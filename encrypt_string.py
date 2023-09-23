bd8505929d3a2fb29e3f49858ea9c9021c989ad3
Maria Campbell <interglobalmedia@gmail.com>
2023-09-23 15:20:26 -0400

    Create function variable

--- encrypt_string.py
+++ encrypt_string.py
@@ -1,16 +1,16 @@
 from cryptography.fernet import Fernet
 from get_key import load_key
 
-# load the key
-crypto_key = load_key()
-# store string in variable
-message = 'super secret message'
-# initialize the Fernet class
-f = Fernet(crypto_key)
-# encrypt data
-message_encrypted = f.encrypt(message.encode())
-print(message_encrypted)
+def message_encrypt():
+  # load the key
+  crypto_key = load_key()
+  # store string in variable
+  message = 'super secret message'
+  # initialize the Fernet class
+  f = Fernet(crypto_key)
+  # encrypt data
+  message_encrypt.message_encrypted = f.encrypt(message.encode())
+  print(message_encrypt.message_encrypted)
 
 # decrypt data
-message_decrypted = f.decrypt(message_encrypted.decode())
-print(message_decrypted)
+message_encrypt()
