6dcce61d556c6e0d9c473fc414f42dc82a2de173
Maria Campbell <interglobalmedia@gmail.com>
2023-09-23 15:20:45 -0400

    Add README file and decrypt_string file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Encrypting/Decrypting Files and Strings
+
+## Python vs Command Line
+
+In cybersecurity, we want to be as efficient and swift as possible when executing our tasks. In this scenario, I want to compare encrypting and decrypting files and strings in Python vs doing the same thing via the Command Line.
---
+++ decrypt_string.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+from get_key import load_key
+from encrypt_string import message_encrypt
+
+def message_decrypt():
+  # load the key
+  crypto_key = load_key()
+  # initialize the Fernet class
+  f = Fernet(crypto_key)
+  # encrypt data
+  message_encrypt.message_encrypted = f.decrypt(message_encrypt.message_encrypted.decode())
+
+message_decrypt()
+print(message_encrypt.message_encrypted)
